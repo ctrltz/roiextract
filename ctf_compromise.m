a2b7c7b8581a27d07cb3ac702de6fb7cd4ed98a9
Nicholas Kapralov <4dvlup@gmail.com>
2023-05-03 09:39:09 +0200

    Remove everything related to Matlab

--- ctf_compromise.m
+++
@@ -1,9 +0,0 @@
-function [F, dF, F_dp_adj, F_rat_adj] = ctf_compromise(w, L, w0, I_in, alpha)
-%CTF_COMPROMISE Optimize dotprod within ROI and in/out ratio simultaneously
-    [F_dp, dF_dp, F_dp_adj] = ctf_dotprod_within(w, L, w0, I_in);
-    [F_rat, dF_rat, F_rat_adj] = ctf_ratio(w, L, I_in);
-
-    F = alpha * F_dp + (1 - alpha) * F_rat;
-    dF = alpha * dF_dp + (1 - alpha) * dF_rat;
-end
-
--- roiextract.py
+++ roiextract.py
@@ -1,6 +1,8 @@
 import numpy as np
 
+from mne.label import label_sign_flip
 from numpy.linalg import norm
+from scipy.linalg import eig
 from scipy.optimize import minimize
 
 
@@ -118,7 +120,7 @@ def ctf_quantify(w, L, w0, mask):
     return dp, rat
 
 
-def ctf_optimize(L, w0, mask, alpha, x0, **kwargs):
+def ctf_optimize_numerical(L, w0, mask, alpha, x0, **kwargs):
     """
     Optimize CTF-based properties (dot product with a template or in/out ratio) for a spatial filter
 
@@ -153,3 +155,87 @@ def ctf_optimize(L, w0, mask, alpha, x0, **kwargs):
         raise ValueError('Value of alpha should lie in [0, 1] range')
 
     return minimize(_ctf_compromise, x0, args=(L, w0, mask, alpha), jac=True, **kwargs)
+
+
+def ctf_optimize(L, w0, mask, alpha, reg=0.000001):
+    """
+    Optimize CTF-based properties (dot product with a template or in/out ratio) for a spatial filter
+
+    Parameters
+    ----------
+    L: array_like
+        Lead field matrix for dipoles with fixed orientation with shape (channels, voxels).
+    w0: array_like
+        Template CTF pattern for voxels with shape (voxels,). Only values within the ROI are used.
+    mask: array_like
+        Voxel mask of the region of interest with shape (voxels,). Contains ones and zeros for voxels within and
+        outside ROI, respectively.
+    alpha: float
+        If 0, only ratio is optimized. If 1, only dot product. Values in between allow tweaking the balance between
+        optimization for dot product or ratio.
+    reg: float, default is 0.000001
+        Regularization parameter to ensure that it is possible to calculate the inverse matrices.
+
+    Returns
+    -------
+    w: array
+        Spatial filter produced by the optimization.
+
+    Raises
+    ------
+    ValueError
+        If provided alpha value is out of [0, 1] range.
+    """
+    if alpha > 1.0 or alpha < 0.0:
+        raise ValueError('Value of alpha should lie in [0, 1] range')
+
+    # Split lead field matrix into in and out parts
+    L_in = L[:, mask]
+    L_out = L[:, ~mask]
+
+    # Solve the generalized eigenvalue problem
+    A = (1 - alpha) * (L_out @ L_out.T) - alpha * L_in @ (w0.T @ w0) @ L_in.T
+    A_reg = A + reg * np.trace(A) * np.eye(*A.shape) / A.shape[0]
+    B = L_in @ L_in.T
+    B_reg = B + reg * np.trace(B) * np.eye(*B.shape) / B.shape[0]
+    [eigvals, eigvecs] = eig(A_reg, B_reg)
+
+    # Get the eigenvector that corresponds to the smallest eigenvalue
+    w = eigvecs[:, eigvals.argmin()]
+    if np.dot(w @ L_in, w0.T) < 0:
+        w *= -1
+
+    return w
+
+
+def ctf_optimize_label(L, label, src, w0, alpha, reg=0.00001):
+    # Create a binary mask for the ROI
+    mask = get_label_mask(label, src)
+
+    # Support pre-defined options for w0
+    if isinstance(w0, str):
+        if w0 == 'mean_flip':
+            w0 = label_sign_flip(label, src)[np.newaxis, :]
+        elif w0 == 'svd_leadfield':
+            raise NotImplementedError('svd_leadfield')
+        else:
+            raise ValueError(f'Bad option for template weights: {w0}')
+
+    return ctf_optimize(L, w0, mask, alpha, reg)
+
+
+def get_label_mask(label, src):
+    vertno = [s['vertno'] for s in src]
+    nvert = [len(vn) for vn in vertno]
+    if label.hemi == 'lh':
+        this_vertices = np.intersect1d(vertno[0], label.vertices)
+        vert = np.searchsorted(vertno[0], this_vertices)
+    elif label.hemi == 'rh':
+        this_vertices = np.intersect1d(vertno[1], label.vertices)
+        vert = nvert[0] + np.searchsorted(vertno[1], this_vertices)
+    else:
+        raise ValueError('label %s has invalid hemi' % label.name)
+
+    mask = np.zeros((sum(nvert),), dtype=int)
+    mask[vert] = 1
+    return mask > 0
