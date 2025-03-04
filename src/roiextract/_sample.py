422243ae6ba60f82ede23f014474a6890fe31709
Nikolai Kapralov <4dvlup@gmail.com>
2025-03-04 13:09:52 +0100

    Minor fixes

--- src/roiextract/_sample.py
+++ src/roiextract/_sample.py
@@ -17,8 +17,8 @@ def sample_criterion(lmbd, fwd, label, template, mode, dist_fun, return_filter=F
 
     x_key = "hom" if mode == "homogeneity" else "sim"
     dist = dist_fun(props[x_key], props["rat"])
-    props_parts = [f"{k}: {v:.3g}" for k, v in props]
+    props_parts = [f"{k}: {v:.4g}" for k, v in props.items()]
     props_desc = ", ".join(props_parts)
-    print(f"sample_criterion | lambda={lmbd:.3g} | {props_desc} | dist={dist}")
+    print(f"sample_criterion | lambda={lmbd:.4g} | {props_desc} | dist={dist:.4g}")
 
     return dist
