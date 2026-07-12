7f93f9d8e6e1553e25d5fd9b9aecb4c83f6868ec
Nikolai Kapralov <4dvlup@gmail.com>
2026-07-12 15:36:51 +0200

    ENH: extend label mask generation to BiHemiLabel

--- src/roiextract/prepare.py
+++ src/roiextract/prepare.py
@@ -41,10 +41,35 @@ def prepare_leadfield(fwd: mne.Forward | np.ndarray) -> np.ndarray:
 
 
 def prepare_label_mask(label, fwd=None):
+    """
+    Prepare a binary mask that indicates which sources in the forward model
+    correspond to the given label.
+
+    Parameters
+    ----------
+    label : mne.Label | mne.BiHemiLabel | np.ndarray
+        The label for which to create the mask. If a numpy array is provided,
+        it is assumed to contain such a binary mask and will be returned as is.
+    fwd : mne.Forward, optional
+        The forward model from which to extract the source space. This is required
+        if an :class:`mne.Label` or :class:`mne.BiHemiLabel` is provided as the
+        first argument.
+
+    Returns
+    -------
+    mask : np.ndarray
+        A binary mask indicating which sources in the forward model correspond to
+        the given label.
+    """
     if not isinstance(label, mne.Label | mne.BiHemiLabel):
         return label
 
-    assert fwd is not None and isinstance(fwd, mne.Forward)
+    if fwd is None or not isinstance(fwd, mne.Forward):
+        raise ValueError(
+            "An `mne.Forward` object must be provided when using an `mne.Label` "
+            "or `mne.BiHemiLabel` to define the ROI."
+        )
+
     return get_label_mask(label, fwd["src"])
 
 
